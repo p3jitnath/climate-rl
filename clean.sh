e165195713e297285577a63ead6b23ac30884072
Pritthijit Nath <41519676+nathzi1505@users.noreply.github.com>
2024-04-08 23:27:43 +0100

    added functionality to record episodes at every 100th epsiode

--- clean.sh
+++ clean.sh
@@ -3,4 +3,6 @@ rm -rf videos/*
 rm -rf rl-algos/*/runs/
 rm -rf rl-algos/*/videos/
 rm -rf rl-algos/*/*.out
+rm -rf rl-algos/*/__pycache__
+rm -rf rl-algos/*/.ipynb_checkpoints
 rm -rf wandb/*
--- rl-algos/ddpg/main.py
+++ rl-algos/ddpg/main.py
@@ -72,7 +72,11 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+            env = gym.wrappers.RecordVideo(
+                env,
+                f"videos/{run_name}",
+                episode_trigger=lambda x: x % 100 == 0,
+            )
         else:
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
