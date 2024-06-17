0535085845032e0a7ca8e1f92592aa006474c665
Pritthijit Nath <41519676+nathzi1505@users.noreply.github.com>
2024-06-17 15:27:07 +0100

    moved .sh files to misc

--- misc/clean.sh
+++ misc/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BASE_DIR="/gws/nopw/j04/ai4er/users/pn341/climate-rl"
+
 # Check if a command-line argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <tag_name>"
@@ -10,8 +12,8 @@ fi
 TAG_NAME=$1
 
 # Remove the specified tag from each directory
-rm -rf "runs/$TAG_NAME"*
-rm -rf "videos/$TAG_NAME"*
-rm -rf "slurm"/*.err
-rm -rf "slurm"/*.out
-rm -rf "param_tune/tmp"/*.tmp
+rm -rf "$BASE_DIR/runs/$TAG_NAME"*
+rm -rf "$BASE_DIR/videos/$TAG_NAME"*
+rm -rf "$BASE_DIR/slurm"/*.err
+rm -rf "$BASE_DIR/slurm"/*.out
+rm -rf "$BASE_DIR/param_tune/tmp"/*.tmp
